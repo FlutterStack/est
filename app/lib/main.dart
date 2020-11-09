3c39278a6f35b0c4f256b9ebe5766b3772ac7b68
Angelo Mien <angelo.mien@hoolisoftware.com>
2020-11-09 13:57:22 +0800

    Make a route.dart for routing the screens.

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,14 +1,7 @@
 // import 'package:expert_properties/components/foldables_sidebar.dart';
-import 'package:expert_properties/menu/menuDashboard.dart';
-import 'package:expert_properties/maincreens/agricultural_page.dart';
-import 'package:expert_properties/maincreens/commercial_page.dart';
-import 'package:expert_properties/maincreens/splash_screen.dart';
-import 'package:expert_properties/maincreens/residential_page.dart';
-import 'package:expert_properties/maincreens/to_sell_page.dart';
-import 'package:expert_properties/maincreens/splash_screen.dart';
-import 'package:expert_properties/maincreens/industrial_page.dart';
 import 'package:flutter/material.dart';
 import 'maincreens/first_page.dart';
+import 'package:expert_properties/routes.dart';
 
 void main() => runApp(Properties());
 
@@ -22,16 +15,7 @@ class Properties extends StatelessWidget {
         scaffoldBackgroundColor: Color(0xFF0a0a0a),
       ),
       initialRoute: '/splash',
-      routes: {
-        '/': (context) => FirstPage(context),
-        '/splash': (context) => SplashScreen(),
-        '/residential': (context) => ResidentialPage(),
-        '/commercial': (context) => CommercialPage(),
-        '/industrial': (context) => IndustrialPage(),
-        '/agricultural': (context) => AgriculturalPage(),
-        '/toSell': (context) => ToSellPage(),
-        '/menuDashboard': (context) => MenuDashboard(),
-      },
+      onGenerateRoute: Routes.generateRoute,
     );
   }
 }
---
+++ app/lib/routes.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:expert_properties/menu/menuDashboard.dart';
+import 'package:expert_properties/maincreens/agricultural_page.dart';
+import 'package:expert_properties/maincreens/commercial_page.dart';
+import 'package:expert_properties/maincreens/splash_screen.dart';
+import 'package:expert_properties/maincreens/residential_page.dart';
+import 'package:expert_properties/maincreens/to_sell_page.dart';
+import 'package:expert_properties/maincreens/splash_screen.dart';
+import 'package:expert_properties/maincreens/industrial_page.dart';
+
+import 'maincreens/agricultural_page.dart';
+import 'maincreens/commercial_page.dart';
+import 'maincreens/first_page.dart';
+import 'maincreens/industrial_page.dart';
+import 'maincreens/residential_page.dart';
+import 'maincreens/splash_screen.dart';
+import 'maincreens/to_sell_page.dart';
+import 'menu/menuDashboard.dart';
+
+class Routes {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    final args = settings.arguments;
+
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (context) => FirstPage(context));
+      case '/splash':
+        return MaterialPageRoute(builder: (_) => SplashScreen());
+      case '/residential':
+        return MaterialPageRoute(builder: (_) => ResidentialPage());
+      case '/commercial':
+        return MaterialPageRoute(builder: (_) => CommercialPage());
+      case '/industrial':
+        return MaterialPageRoute(builder: (_) => IndustrialPage());
+      case '/agricultural':
+        return MaterialPageRoute(builder: (_) => AgriculturalPage());
+      case '/toSell':
+        return MaterialPageRoute(builder: (_) => ToSellPage());
+      case '/menuDashboard':
+        return MaterialPageRoute(builder: (_) => MenuDashboard());
+    }
+  }
+
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(
+      builder: (_) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Unknown Route'),
+          ),
+          body: Center(
+            child: Text('ERROR'),
+          ),
+        );
+      },
+    );
+  }
+}
