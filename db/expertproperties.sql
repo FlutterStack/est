3b7313de64238db0c1bc73e3d5ecbd4c790afb59
Angelo Mien <angelo.mien@hoolisoftware.com>
2020-11-10 23:37:19 +0800

    Update expertproperties.sql

--- db/expertproperties.sql
+++ db/expertproperties.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.9.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 08, 2020 at 05:59 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.6
+-- Host: localhost:8889
+-- Generation Time: Nov 10, 2020 at 03:36 PM
+-- Server version: 5.7.26
+-- PHP Version: 7.4.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -24,17 +23,46 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `estate`
+--
+
+CREATE TABLE `estate` (
+  `estate_id` int(10) UNSIGNED NOT NULL,
+  `id_residential` int(11) UNSIGNED NOT NULL,
+  `estate_name` varchar(255) NOT NULL,
+  `estate_location` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `lu_estate_img`
 --
 
 CREATE TABLE `lu_estate_img` (
-  `img_id` int(10) unsigned NOT NULL,
-  `estate_id` int(10) unsigned NOT NULL,
-  `img_path` varchar(255) NOT NULL,
-  PRIMARY KEY (`img_id`),
-  KEY `lu_estate_img_fk_01_idx` (`estate_id`),
-  CONSTRAINT `lu_estate_img_fk_01` FOREIGN KEY (`estate_id`) REFERENCES `estate` (`estate_id`)
+  `img_id` int(10) UNSIGNED NOT NULL,
+  `estate_id` int(10) UNSIGNED NOT NULL,
+  `img_path` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lu_estate_info`
+--
+
+CREATE TABLE `lu_estate_info` (
+  `sub_id` int(10) UNSIGNED NOT NULL,
+  `estate_id` int(10) UNSIGNED NOT NULL,
+  `no_of_rooms` int(15) NOT NULL,
+  `no_of_garage` int(10) DEFAULT NULL,
+  `no_of_kitchens` int(10) NOT NULL,
+  `no_of_bathrooms` int(10) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `size` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 -- --------------------------------------------------------
 
 --
@@ -47,24 +75,16 @@ CREATE TABLE `residential` (
   `description` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `residential`
+--
 
---
--- Table structure for table `lu_estate_info`
---
-CREATE TABLE `lu_estate_info` (
-  `sub_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `estate_id` int(10) unsigned NOT NULL,
-  `no_of_rooms` int(15) NOT NULL,
-  `no_of_garage` int(10) DEFAULT NULL,
-  `no_of_kitchens` int(10) NOT NULL,
-  `no_of_bathrooms` int(10) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `size` varchar(100) NOT NULL,
-  PRIMARY KEY (`sub_id`),
-  KEY `lu_estate_info_fk_01_idx` (`estate_id`),
-  CONSTRAINT `lu_estate_info_fk_01` FOREIGN KEY (`estate_id`) REFERENCES `estate` (`estate_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `residential` (`id_residential`, `class_name`, `description`) VALUES
+(1, 'Residential', 'Residential is simply dummy text of the printing and typesetting industry.'),
+(2, 'Commercial', 'Commercial is simply dummy text of the printing and typesetting industry.'),
+(3, 'Industrial', 'Industrial is simply dummy text of the printing and typesetting industry.'),
+(4, 'Agricultural', 'Agricultural is simply dummy text of the printing and typesetting industry.');
+
 -- --------------------------------------------------------
 
 --
@@ -72,51 +92,51 @@ CREATE TABLE `lu_estate_info` (
 --
 
 CREATE TABLE `users` (
-  `idusers` int(10) NOT NULL AUTO_INCREMENT,
+  `idusers` int(10) NOT NULL,
   `first_name` varchar(45) NOT NULL,
   `last_name` varchar(45) NOT NULL,
   `email` varchar(45) NOT NULL,
   `mobile_no` int(12) DEFAULT NULL,
-  `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`idusers`)
+  `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Table structure for table `estate`
+-- Dumping data for table `users`
 --
 
-CREATE TABLE `estate` (
-  `estate_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `id_residential` int(11) unsigned NOT NULL,
-  `estate_name` varchar(255) NOT NULL,
-  `estate_location` varchar(255) NOT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`estate_id`),
-  KEY `estate_fk_01_idx` (`id_residential`),
-  CONSTRAINT `estate_fk_01` FOREIGN KEY (`id_residential`) REFERENCES `residential` (`id_residential`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO `users` (`idusers`, `first_name`, `last_name`, `email`, `mobile_no`, `password`) VALUES
+(1, 'John', 'Doe', 'johndoe@gmail.com', NULL, 'test123');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `estate`
+--
+ALTER TABLE `estate`
+  ADD PRIMARY KEY (`estate_id`),
+  ADD KEY `estate_fk_01_idx` (`id_residential`);
+
+--
 -- Indexes for table `lu_estate_img`
 --
 ALTER TABLE `lu_estate_img`
-  ADD PRIMARY KEY (`img_id`);
+  ADD PRIMARY KEY (`img_id`),
+  ADD KEY `lu_estate_img_fk_01_idx` (`estate_id`);
+
+--
+-- Indexes for table `lu_estate_info`
+--
+ALTER TABLE `lu_estate_info`
+  ADD PRIMARY KEY (`sub_id`),
+  ADD KEY `lu_estate_info_fk_01_idx` (`estate_id`);
 
 --
 -- Indexes for table `residential`
 --
 ALTER TABLE `residential`
   ADD PRIMARY KEY (`id_residential`);
-
---
--- Indexes for table `lu_estate_info`
---
-ALTER TABLE `lu_estate_info`
-  ADD PRIMARY KEY (`sub_id`);
 
 --
 -- Indexes for table `users`
@@ -129,40 +149,51 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `estate`
+--
+ALTER TABLE `estate`
+  MODIFY `estate_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `lu_estate_info`
+--
+ALTER TABLE `lu_estate_info`
+  MODIFY `sub_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `residential`
 --
 ALTER TABLE `residential`
-  MODIFY `id_residential` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id_residential` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `subclass`
+-- AUTO_INCREMENT for table `users`
 --
-ALTER TABLE `subclass`
-  MODIFY `sub_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-COMMIT;
+ALTER TABLE `users`
+  MODIFY `idusers` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `estate`
+--
+ALTER TABLE `estate`
+  ADD CONSTRAINT `estate_fk_01` FOREIGN KEY (`id_residential`) REFERENCES `residential` (`id_residential`);
+
+--
+-- Constraints for table `lu_estate_img`
+--
+ALTER TABLE `lu_estate_img`
+  ADD CONSTRAINT `lu_estate_img_fk_01` FOREIGN KEY (`estate_id`) REFERENCES `estate` (`estate_id`);
+
+--
+-- Constraints for table `lu_estate_info`
+--
+ALTER TABLE `lu_estate_info`
+  ADD CONSTRAINT `lu_estate_info_fk_01` FOREIGN KEY (`estate_id`) REFERENCES `estate` (`estate_id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-
-ALTER TABLE `expertproperties`.`subclass` 
-ADD INDEX `subclass_fk_01_idx` (`id_residential` ASC);
-;
-ALTER TABLE `expertproperties`.`subclass` 
-ADD CONSTRAINT `subclass_fk_01`
-  FOREIGN KEY (`id_residential`)
-  REFERENCES `expertproperties`.`residential` (`id_residential`)
-  ON DELETE RESTRICT
-  ON UPDATE RESTRICT;
-
-
-ALTER TABLE `expertproperties`.`imgsub` 
-ADD INDEX `imgsub_fk_01_idx` (`sub_id` ASC);
-;
-ALTER TABLE `expertproperties`.`imgsub` 
-ADD CONSTRAINT `imgsub_fk_01`
-  FOREIGN KEY (`sub_id`)
-  REFERENCES `expertproperties`.`subclass` (`sub_id`)
-  ON DELETE RESTRICT
-  ON UPDATE RESTRICT;
